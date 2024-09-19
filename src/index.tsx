1594522f7e080f3113c6860609bc6778d962bb66
Plinio Correa <plinio@squareup.com>
2024-09-18 23:05:42 -0700

    Empty react native modules with example app.

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'mobile-payments-sdk-react-native' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const MobilePaymentsSdkReactNative = NativeModules.MobilePaymentsSdkReactNative
+  ? NativeModules.MobilePaymentsSdkReactNative
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return MobilePaymentsSdkReactNative.multiply(a, b);
+}
