30a4c13792e5ff9e22a3bc891b80eaffb279c6fb
Plinio Correa <plinio@squareup.com>
2024-09-30 15:45:21 -0700

    Renamed features to managers

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ export * from './base_sdk';
 export * from './models/enums';
 export * from './models/errors';
 export * from './models/objects';
-export * from './features/auth';
+export * from './managers/auth';
