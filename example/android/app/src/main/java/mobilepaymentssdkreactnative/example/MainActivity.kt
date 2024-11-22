249ee50e9620bc77c3d8d26af61d0ed0ccb3cb92
Nilay-squareup <nilay@squareup.com>
2024-11-22 14:54:57 +0530

    remove unnecessary space

--- example/android/app/src/main/java/mobilepaymentssdkreactnative/example/MainActivity.kt
+++ example/android/app/src/main/java/mobilepaymentssdkreactnative/example/MainActivity.kt
@@ -19,5 +19,5 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-      
+
 }
