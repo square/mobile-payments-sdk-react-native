05872c5a33f69f44ce064b3900dffd2b7a20f8d3
Artem Chubaryan <artem@squareup.com>
2025-01-16 19:09:55 -0600

    Android: Wiring payment, auth, settings - Wired `deauthorize`, `getAuthorizedLocation`, `getAuthorizationState`, `getEnvironment`, `getSdkVersion` - Wired `startPayment` and `cancelPayment` - Updated `showMockReaderUI` to check for login state - Fixed crash due to missing `MOBILE_PAYMENT_SDK_APPLICATION_ID` in `MainApplication`, added a constant for it

--- android/src/main/java/com/mobilepaymentssdkreactnative/MobilePaymentsSdkReactNativeModule.kt
+++ android/src/main/java/com/mobilepaymentssdkreactnative/MobilePaymentsSdkReactNativeModule.kt
@@ -9,33 +9,46 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableMap
 import com.squareup.sdk.mobilepayments.MobilePaymentsSdk
 import com.squareup.sdk.mobilepayments.authorization.AuthorizeErrorCode
-import com.squareup.sdk.mobilepayments.core.Result
+import com.squareup.sdk.mobilepayments.core.Result.Failure
+import com.squareup.sdk.mobilepayments.core.Result.Success
 import com.squareup.sdk.mobilepayments.mockreader.ui.MockReaderUI
+import com.squareup.sdk.mobilepayments.payment.PaymentHandle
+import com.squareup.sdk.mobilepayments.payment.PaymentHandle.CancelResult.CANCELED
+import com.squareup.sdk.mobilepayments.payment.PaymentHandle.CancelResult.NOT_CANCELABLE
+import com.squareup.sdk.mobilepayments.payment.PaymentHandle.CancelResult.NO_PAYMENT_IN_PROGRESS
 
 class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+
+  private var paymentHandle: PaymentHandle? = null
 
   override fun getName(): String {
     return NAME
   }
 
   @ReactMethod
-  fun authorize(accessToken: String, locationId: String, promise: Promise) {
+  fun authorize(
+    accessToken: String,
+    locationId: String,
+    promise: Promise
+  ) {
     val authorizationManager = MobilePaymentsSdk.authorizationManager()
 
-    if(authorizationManager.authorizationState.isAuthorized){
+    if (authorizationManager.authorizationState.isAuthorized) {
       return
     }
     authorizationManager.authorize(accessToken, locationId) { result ->
       when (result) {
-        is Result.Success -> {
+        is Success -> {
           finishWithAuthorizedSuccess(reactContext, result.value)
           promise.resolve("Authorized with token: $accessToken and location: $locationId")
         }
-        is Result.Failure<*, *> -> { // Match any Failure type
-          handleAuthorizationFailure(result as Result.Failure<AuthorizeErrorCode, *>, promise)
+
+        is Failure<*, *> -> { // Match any Failure type
+          handleAuthorizationFailure(result as Failure<AuthorizeErrorCode, *>, promise)
         }
       }
     }
@@ -43,17 +56,25 @@ class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicat
 
   @ReactMethod
   fun deauthorize(promise: Promise) {
-    promise.resolve("Not yet implemented")
+    MobilePaymentsSdk.authorizationManager().deauthorize()
+    promise.resolve("Square Mobile Payments SDK successfully deauthorized.")
   }
 
   @ReactMethod
   fun getAuthorizedLocation(promise: Promise) {
-    promise.resolve("Not yet implemented")
+    val authorizationManager = MobilePaymentsSdk.authorizationManager()
+    promise.resolve(authorizationManager.location?.toLocationMap())
   }
 
   @ReactMethod
   fun getAuthorizationState(promise: Promise) {
-    promise.resolve("Not yet implemented")
+    val authorizationManager = MobilePaymentsSdk.authorizationManager()
+    val authorizationState = when {
+      authorizationManager.authorizationState.isAuthorizationInProgress -> "AUTHORIZING"
+      authorizationManager.authorizationState.isAuthorized -> "AUTHORIZED"
+      else -> "NOT_AUTHORIZED"
+    }
+    promise.resolve(authorizationState)
   }
 
   @ReactMethod
@@ -62,13 +83,17 @@ class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicat
       val settingsManager = MobilePaymentsSdk.settingsManager()
       settingsManager.showSettings { result ->
         when (result) {
-          is Result.Success -> {
+          is Success -> {
             // Ensure that the value can be resolved correctly
             promise.resolve("Settings closed successfully")
           }
-          is Result.Failure -> {
+
+          is Failure -> {
             // Handle failure
-            promise.reject(result.errorCode.toString(), result.errorMessage ?: "settings screen can't be presented")
+            promise.reject(
+              result.errorCode.toString(),
+              result.errorMessage
+            )
           }
         }
       }
@@ -76,8 +101,21 @@ class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicat
   }
 
   @ReactMethod
+  fun getEnvironment(promise: Promise) {
+    val settingsManager = MobilePaymentsSdk.settingsManager()
+    promise.resolve(settingsManager.getSdkSettings().sdkEnvironment.name)
+  }
+
+  @ReactMethod
+  fun getSdkVersion(promise: Promise) {
+    val settingsManager = MobilePaymentsSdk.settingsManager()
+    promise.resolve(settingsManager.getSdkSettings().sdkVersion)
+  }
+
+  @ReactMethod
   fun showMockReaderUI(promise: Promise) {
-    if (MobilePaymentsSdk.isSandboxEnvironment()) {
+    if (MobilePaymentsSdk.isSandboxEnvironment() &&
+      MobilePaymentsSdk.authorizationManager().authorizationState.isAuthorized) {
       reactContext.currentActivity?.runOnUiThread {
         MockReaderUI.show()
       }
@@ -90,27 +128,88 @@ class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicat
     MockReaderUI.hide()
   }
 
+  @ReactMethod
+  fun startPayment(
+    paymentParameters: ReadableMap,
+    promptParameters: ReadableMap,
+    promise: Promise
+  ) {
+    val paymentManager = MobilePaymentsSdk.paymentManager()
+    val parsedPaymentParameters = try {
+      paymentParameters.readPaymentParameters()
+    } catch (e: IllegalArgumentException) {
+      promise.reject("INVALID_PAYMENT_PARAMETERS", e.message)
+      return
+    }
+
+    val parsedPromptParameters = try {
+      promptParameters.readPromptParameters()
+    } catch (e: IllegalArgumentException) {
+      promise.reject("INVALID_PAYMENT_PROMPT", e.message)
+      return
+    }
+
+    if (paymentHandle != null) {
+      promise.reject("PAYMENT_IN_PROGRESS", "A payment is already in progress")
+      return
+    }
+
+    reactContext.runOnUiQueueThread {
+      paymentHandle = paymentManager.startPaymentActivity(
+        parsedPaymentParameters,
+        parsedPromptParameters
+      ) { result ->
+        paymentHandle = null
+        when (result) {
+          is Failure -> promise.reject("PAYMENT_FAILED", result.errorMessage)
+          is Success -> promise.resolve(result.value.toPaymentMap())
+        }
+      }
+    }
+  }
+
+  @ReactMethod
+  fun cancelPayment(promise: Promise) {
+    val cancelResult = paymentHandle?.cancel()
+    when (cancelResult) {
+      CANCELED -> promise.resolve("Payment successfully canceled")
+      NOT_CANCELABLE ->
+        promise.reject("PAYMENT_CANCEL_ERROR", "This payment cannot be canceled.")
+      NO_PAYMENT_IN_PROGRESS, null ->
+        promise.reject("PAYMENT_CANCEL_ERROR", "No payment available to cancel.")
+    }
+    paymentHandle = null
+  }
+
   companion object {
     const val NAME = "MobilePaymentsSdkReactNative"
   }
 
-  private fun handleAuthorizationFailure(result: Result.Failure<AuthorizeErrorCode, *>, promise: Promise) {
+  private fun handleAuthorizationFailure(
+    result: Failure<AuthorizeErrorCode, *>,
+    promise: Promise
+  ) {
     when (result.errorCode) {
       AuthorizeErrorCode.NO_NETWORK -> {
         showRetryDialog(reactContext, result)
         Log.d("MobilePayments", "Authorization Failed: $result")
       }
+
       AuthorizeErrorCode.USAGE_ERROR -> {
         showUsageErrorDialog(reactContext, result)
         Log.d("MobilePayments", "Authorization Error: $result")
       }
+
       else -> {
         promise.reject("AUTHORIZATION_ERROR", "Authorization failed: ${result.errorCode}")
       }
     }
   }
 
-  private fun showRetryDialog(context: Context, result: Result.Failure<AuthorizeErrorCode, *>) {
+  private fun showRetryDialog(
+    context: Context,
+    result: Failure<AuthorizeErrorCode, *>
+  ) {
     if (context is Activity && !context.isFinishing) {
       val builder = AlertDialog.Builder(context)
       builder.setTitle("Network Error")
@@ -123,18 +222,25 @@ class MobilePaymentsSdkReactNativeModule(private val reactContext: ReactApplicat
     }
   }
 
-  private fun showUsageErrorDialog(context: Context, result: Result.Failure<AuthorizeErrorCode, *>) {
+  private fun showUsageErrorDialog(
+    context: Context,
+    result: Failure<AuthorizeErrorCode, *>
+  ) {
     if (context is Activity && !context.isFinishing) {
       val builder = AlertDialog.Builder(context)
       builder.setTitle("Usage Error")
-        .setMessage("There was an error with the usage of the payment system. Please check your settings.")
+        .setMessage(
+          "There was an error with the usage of the payment system. Please check your settings."
+        )
         .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
       builder.create().show()
     }
   }
 
-
-  private fun finishWithAuthorizedSuccess(context: Context, value: Any) {
+  private fun finishWithAuthorizedSuccess(
+    context: Context,
+    value: Any
+  ) {
     Toast.makeText(context, "Authorization successful: $value", Toast.LENGTH_LONG).show()
   }
 }
