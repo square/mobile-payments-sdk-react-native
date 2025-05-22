0ac32ce9829a16f76c75835b796b413429b85bba
Cesar Sosa <101717495+cesar-sosa-hol@users.noreply.github.com>
2025-05-22 18:53:33 +0200

    adding StatusBar to be fixed and always visible (#48)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,26 +2,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeView from './Screens/HomeScreen';
 import PermissionsScreen from './Screens/PermissionsScreen';
+import { StatusBar } from 'react-native';
 import TestScreen from './Screens/TestScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeView}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Permissions"
-          component={PermissionsScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Test" component={TestScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar backgroundColor="#fff" translucent={false} hidden={false} />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Permissions"
+            component={PermissionsScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Test" component={TestScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
