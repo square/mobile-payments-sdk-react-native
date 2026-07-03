fb1f392fb86738031bab65782d290476f05894cb
Cesar Sosa <cesarsosa.p@gmail.com>
2026-07-03 16:05:05 +0200

    fix APP Id

--- example-expo/app.plugin.js
+++ example-expo/app.plugin.js
@@ -7,7 +7,10 @@ const {
   withInfoPlist,
   createRunOncePlugin,
   withGradleProperties,
+  withDangerousMod,
 } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 const pkg = require('./package.json');
 
@@ -27,10 +30,11 @@ const SQUARE_ACCESSORY_PROTOCOLS = [
 ];
 
 const withSquareMavenRepository = (config) => {
-  return withProjectBuildGradle(config, (config) => {
-    if (config.modResults.language === 'groovy') {
-      const buildGradle = config.modResults.contents;
-      const mavenUrl = 'maven { url("https://sdk.squareup.com/public/android/") }';
+  return withProjectBuildGradle(config, (modConfig) => {
+    if (modConfig.modResults.language === 'groovy') {
+      const buildGradle = modConfig.modResults.contents;
+      const mavenUrl =
+        'maven { url("https://sdk.squareup.com/public/android/") }';
       const kotlinArgs = `
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
         compilerOptions {
@@ -54,17 +58,18 @@ const withSquareMavenRepository = (config) => {
         );
       }
 
-      config.modResults.contents = newContents;
+      modConfig.modResults.contents = newContents;
     }
-    return config;
+    return modConfig;
   });
 };
 
 const withSquareMainApplication = (config, { applicationId }) => {
-  return withMainApplication(config, (config) => {
-    if (config.modResults.language === 'kt') {
-      let contents = config.modResults.contents;
-      const importStatement = 'import com.squareup.sdk.mobilepayments.MobilePaymentsSdk';
+  return withMainApplication(config, (modConfig) => {
+    if (modConfig.modResults.language === 'kt') {
+      let contents = modConfig.modResults.contents;
+      const importStatement =
+        'import com.squareup.sdk.mobilepayments.MobilePaymentsSdk';
 
       if (!contents.includes(importStatement)) {
         contents = contents.replace(
@@ -82,15 +87,15 @@ const withSquareMainApplication = (config, { applicationId }) => {
         );
       }
 
-      config.modResults.contents = contents;
+      modConfig.modResults.contents = contents;
     }
-    return config;
+    return modConfig;
   });
 };
 
 const withSquareAndroidPermissions = (config) => {
-  return withAndroidManifest(config, (config) => {
-    const manifest = config.modResults.manifest;
+  return withAndroidManifest(config, (modConfig) => {
+    const manifest = modConfig.modResults.manifest;
 
     if (!manifest['uses-permission']) {
       manifest['uses-permission'] = [];
@@ -105,13 +110,16 @@ const withSquareAndroidPermissions = (config) => {
       }
     }
 
-    return config;
+    return modConfig;
   });
 };
 
-const withSquareIosPlist = (config, { applicationId, accessToken, locationId }) => {
-  return withInfoPlist(config, (config) => {
-    const plist = config.modResults;
+const withSquareIosPlist = (
+  config,
+  { applicationId, accessToken, locationId }
+) => {
+  return withInfoPlist(config, (modConfig) => {
+    const plist = modConfig.modResults;
 
     if (!plist.UISupportedExternalAccessoryProtocols) {
       plist.UISupportedExternalAccessoryProtocols = [];
@@ -152,34 +160,65 @@ const withSquareIosPlist = (config, { applicationId, accessToken, locationId }) 
     if (accessToken) plist.ACCESS_TOKEN = accessToken;
     if (locationId) plist.LOCATION_ID = locationId;
 
-    return config;
+    return modConfig;
   });
 };
 
 const withSquareAppDelegate = (config) => {
-  return withAppDelegate(config, (config) => {
-    let contents = config.modResults.contents;
+  return withAppDelegate(config, (modConfig) => {
+    let contents = modConfig.modResults.contents;
+    const isSwift =
+      modConfig.modResults.language === 'swift' ||
+      contents.includes('ExpoAppDelegate');
 
-    const initCall = 'MPRNInitializeSquareSDK(launchOptions, [[NSBundle mainBundle] objectForInfoDictionaryKey:@"APP_ID"])';
+    if (isSwift) {
+      if (!contents.includes('import SquareMobilePaymentsSDK')) {
+        contents = contents.replace(
+          /import ReactAppDependencyProvider/,
+          'import ReactAppDependencyProvider\nimport SquareMobilePaymentsSDK'
+        );
+      }
 
-    if (!contents.includes('MPRNInitializeSquareSDK')) {
+      contents = contents.replace(
+        /\n\s*MPRNInitializeSquareSDK\(launchOptions, \[\[NSBundle mainBundle\] objectForInfoDictionaryKey:@"APP_ID"\]\);\n?/,
+        '\n'
+      );
+
+      if (!contents.includes('MobilePaymentsSDK.initialize')) {
+        contents = contents.replace(
+          /didFinishLaunchingWithOptions[^{]*\{/,
+          `$&
+    MobilePaymentsSDK.initialize(
+      applicationLaunchOptions: launchOptions,
+      squareApplicationID: Bundle.main
+        .object(forInfoDictionaryKey: "APP_ID") as! String
+    )
+`
+        );
+      }
+    } else if (!contents.includes('MPRNInitializeSquareSDK')) {
+      const initCall =
+        'MPRNInitializeSquareSDK(launchOptions, [[NSBundle mainBundle] objectForInfoDictionaryKey:@"APP_ID"])';
+
       contents = contents.replace(
         /didFinishLaunchingWithOptions[^{]*\{/,
         `$&\n  ${initCall};`
       );
     }
 
-    config.modResults.contents = contents;
-    return config;
+    modConfig.modResults.contents = contents;
+    return modConfig;
   });
 };
 
 const withSquareAppBuildGradle = (config, { accessToken, locationId }) => {
-  return withAppBuildGradle(config, (config) => {
-    if (config.modResults.language === 'groovy') {
-      let contents = config.modResults.contents;
-      const mpsdkDep = 'implementation("com.squareup.sdk:mobile-payments-sdk:2.5.0")';
-      const vectorIconsFonts = 'apply from: new File(["node", "--print", "require.resolve(\'react-native-vector-icons/package.json\')"].execute(null, rootDir).text.trim()).getParentFile().absolutePath + "/fonts.gradle"';
+  return withAppBuildGradle(config, (modConfig) => {
+    if (modConfig.modResults.language === 'groovy') {
+      let contents = modConfig.modResults.contents;
+      const mpsdkDep =
+        'implementation("com.squareup.sdk:mobile-payments-sdk:2.5.0")';
+      const vectorIconsFonts =
+        'apply from: new File(["node", "--print", "require.resolve(\'react-native-vector-icons/package.json\')"].execute(null, rootDir).text.trim()).getParentFile().absolutePath + "/fonts.gradle"';
 
       if (accessToken && locationId) {
         const accessTokenField = `buildConfigField "String", "ACCESS_TOKEN", "\\"${accessToken}\\""`;
@@ -200,37 +239,145 @@ const withSquareAppBuildGradle = (config, { accessToken, locationId }) => {
         );
       }
 
-      if (!contents.includes('react-native-vector-icons') && !contents.includes('fonts.gradle')) {
+      if (
+        !contents.includes('react-native-vector-icons') &&
+        !contents.includes('fonts.gradle')
+      ) {
         contents += `\n${vectorIconsFonts}\n`;
       }
 
-      config.modResults.contents = contents;
+      modConfig.modResults.contents = contents;
     }
-    return config;
+    return modConfig;
   });
 };
 
 const withSquarePackagingOptions = (config) => {
-  return withGradleProperties(config, (config) => {
+  return withGradleProperties(config, (modConfig) => {
     const key = 'android.packagingOptions.pickFirsts';
     const val = 'META-INF/versions/9/OSGI-INF/MANIFEST.MF';
-    const index = config.modResults.findIndex(item => item.key === key);
+    const index = modConfig.modResults.findIndex((item) => item.key === key);
 
     if (index > -1) {
-      if (!config.modResults[index].value.includes(val)) {
-        config.modResults[index].value += `,${val}`;
+      if (!modConfig.modResults[index].value.includes(val)) {
+        modConfig.modResults[index].value += `,${val}`;
       }
     } else {
-      config.modResults.push({ type: 'property', key, value: val });
+      modConfig.modResults.push({ type: 'property', key, value: val });
     }
 
-    return config;
+    return modConfig;
   });
+};
+
+const IOS_PODFILE_SQUARE_SETUP_PATCH = `
+def add_square_setup_build_phase(installer)
+  phase_name = '[SquareMobilePaymentsSDK] setup'
+  script = <<-'SCRIPT'
+SETUP_SCRIPT="\${BUILT_PRODUCTS_DIR}/\${FRAMEWORKS_FOLDER_PATH}/SquareMobilePaymentsSDK.framework/setup"
+if [ -f "$SETUP_SCRIPT" ]; then
+  "$SETUP_SCRIPT"
+fi
+SCRIPT
+
+  installer.aggregate_targets.each do |aggregate_target|
+    user_project = aggregate_target.user_project
+    next unless user_project
+
+    user_project.native_targets.each do |native_target|
+      next unless native_target.product_type == 'com.apple.product-type.application'
+
+      existing_phase = native_target.shell_script_build_phases.find { |phase| phase.name == phase_name }
+      existing_phase&.remove_from_project
+
+      phase = native_target.new_shell_script_build_phase(phase_name)
+      phase.shell_script = script
+      phase.always_out_of_date = '1'
+    end
+
+    user_project.save
+  end
+end
+`;
+
+const IOS_PODFILE_FMT_PATCH = `
+def patch_fmt_for_xcode26(installer)
+  fmt_base = File.join(installer.sandbox.root, 'fmt', 'include', 'fmt', 'base.h')
+  return unless File.exist?(fmt_base)
+
+  content = File.read(fmt_base)
+  return if content.include?('Xcode 26 workaround')
+
+  patched = content.gsub(
+    /(#elif defined\\(__cpp_consteval\\)\\n#  define FMT_USE_CONSTEVAL) 1/,
+    "// Xcode 26 workaround: disable consteval\\n\\\\1 0"
+  )
+
+  return if patched == content
+
+  File.chmod(0644, fmt_base)
+  File.write(fmt_base, patched)
+end
+${IOS_PODFILE_SQUARE_SETUP_PATCH}
+`;
+
+const withIosPodfileWorkarounds = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (modConfig) => {
+      const podfilePath = path.join(
+        modConfig.modRequest.platformProjectRoot,
+        'Podfile'
+      );
+
+      if (!fs.existsSync(podfilePath)) {
+        return modConfig;
+      }
+
+      let contents = fs.readFileSync(podfilePath, 'utf8');
+
+      if (
+        !contents.includes('def patch_fmt_for_xcode26') ||
+        !contents.includes('def add_square_setup_build_phase')
+      ) {
+        if (!contents.includes('def patch_fmt_for_xcode26')) {
+          contents = contents.replace(
+            'prepare_react_native_project!',
+            `${IOS_PODFILE_FMT_PATCH}\nprepare_react_native_project!`
+          );
+        } else if (!contents.includes('def add_square_setup_build_phase')) {
+          contents = contents.replace(
+            'prepare_react_native_project!',
+            `${IOS_PODFILE_SQUARE_SETUP_PATCH}\nprepare_react_native_project!`
+          );
+        }
+      }
+
+      if (!/^\s+patch_fmt_for_xcode26\(installer\)/m.test(contents)) {
+        contents = contents.replace(
+          /(\s+)react_native_post_install\(/,
+          '$1patch_fmt_for_xcode26(installer)\n\n$1react_native_post_install('
+        );
+      }
+
+      if (!/^\s+add_square_setup_build_phase\(installer\)/m.test(contents)) {
+        contents = contents.replace(
+          /(\s+react_native_post_install\(\n[\s\S]*?\n\s+\)\n)/,
+          '$1\n    add_square_setup_build_phase(installer)\n'
+        );
+      }
+
+      fs.writeFileSync(podfilePath, contents);
+      return modConfig;
+    },
+  ]);
 };
 
 const withSquareMobilePayments = (config, options) => {
   if (!options || !options.applicationId) {
-    throw new Error("You must provide an 'applicationId' for the Mobile Payments SDK plugin in app.json");
+    throw new Error(
+      "You must provide an 'applicationId' for the Mobile Payments SDK plugin in app.json"
+    );
   }
 
   config = withSquareAppBuildGradle(config, options);
@@ -240,6 +387,7 @@ const withSquareMobilePayments = (config, options) => {
   config = withSquareAppDelegate(config);
   config = withSquareIosPlist(config, options);
   config = withSquarePackagingOptions(config);
+  config = withIosPodfileWorkarounds(config);
 
   return config;
 };
--- example-expo/eslint.config.js
+++ example-expo/eslint.config.js
@@ -5,6 +5,19 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  },
+  {
     ignores: ['dist/*'],
   },
 ]);
